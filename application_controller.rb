a25e5717ae5d318328451c498f45cb47335547e5
Gdelpriore <gianluca.delpriore@gmail.com>
2015-07-31 13:45:44 +0000

    first draft

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,4 @@
+require_relative 'models/model.rb'
 require 'bundler'
 Bundler.require
 
@@ -6,5 +7,28 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  post '/Start' do
+    erb :Start
+  end
+  get'/cornered' do
+    
+    erb :cornered
+  end
+post '/fight' do
+  
+    erb :fight
+  end
 
+post '/next' do
+  @x=roll 
+  if @x>=4
+    erb :win
+  else
+    erb :death
+    
+  end
 end
+  
+end
+  
+  
---
+++ models/model.rb
@@ -0,0 +1,9 @@
+def roll
+  rand(6) + 1
+end
+def jump
+  sleep (5)
+end
+# def x
+#   @x=@c
+# end
